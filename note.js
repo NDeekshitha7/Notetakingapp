1429e35b9a2770a92d6de764522671cea9f0403d
NDeekshitha7 <115596359+NDeekshitha7@users.noreply.github.com>
2022-12-14 14:39:37 -0500

    Add files via upload

---
+++ note.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const note = require('../models/note');
+const router = express.Router();
+
+router
+  .get('/', async (req, res) => {
+    try {
+      const notes = await note.getAllnote();
+      res.send(notes);
+    } catch(err) {
+      res.status(401).send({message: err.message});
+    }
+  })
+
+
+  .post('/register', async (req, res) => {
+    try {
+      let note = await note.register(req.body);
+      res.send({...note, password: undefined})
+    } catch(err) {
+      res.status(401).send({message: err.message});
+    }
+  })
+
+  .put('/edit', async (req, res) => {
+    try {
+      let note = await note.editnote(req.body);
+      res.send({...note, password: undefined});
+    } catch(err) {
+      res.status(401).send({message: err.message})
+    }
+  })
+
+  .delete('/delete', async (req, res) => {
+    try {
+      User.deletenote(req.body);
+      res.send({success: "We'll Miss You... :("})
+    } catch(err) {
+      res.status(401).send({message: err.message})
+    }
+  })
+
+
+
+  
+module.exports = router;
